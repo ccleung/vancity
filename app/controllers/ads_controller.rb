f9a9965d41420da8316522bf9f2aca817e398fb4
ccleung <clemsquared@gmail.com>
2014-08-17 19:15:36 -0400

    render stuff

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -5,7 +5,9 @@ end
 
 def create
 	#render plain: params[:ad].inspect
+	#render plain: current_user.id
 	@ad = Ad.new(ad_params)
+	@ad.user_id = current_user.id
 	@ad.save
 	redirect_to @ad
 end
@@ -18,5 +20,5 @@ private
 	def ad_params
 		params.require(:ad).permit(:title, :description)
 	end
-
+	before_action :authenticate_user!
 end
