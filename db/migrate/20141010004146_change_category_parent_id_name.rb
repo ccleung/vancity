2bb2e214982f34fe57f0cc8323d114d94598e14c
ccleung <clemsquared@gmail.com>
2014-11-19 23:28:12 -0500

    seed db

--- db/migrate/20141010004146_change_category_parent_id_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCategoryParentIdName < ActiveRecord::Migration
-  def change
-  	rename_column :categories, :category_id, :parent_id
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,46 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+puts 'Creating admin... ' << user.email
 
 location = Location.find_or_create_by(city: 'Toronto', region: 'ON', country: 'Canada')
 
-puts 'CREATED LOCATION: ' << location.city
+puts 'Creating location... ' << location.city
 
-Category.create([{ name: 'Services' }, { name: 'Personal' }, { name: 'Other' }])
+location = Location.find_or_create_by(city: 'Toronto', region: 'ON', country: 'Canada')
+
+root_category_names = ['goods', 'services']
+
+#services = Category.find_or_create_by(name: 'Services')
+#goods = Category.find_or_create_by(name: 'Goods')
+
+sub_category_names = {
+	root_category_names[0] => ['books', 'computers', 'electronics', 'furniture', 'other'],
+	root_category_names[1] => ['photographer', 'ux designer', 'software developer', 'DJ', 'videographer', 'other']
+}
+
+root_categories = []
+
+for root_category_name in root_category_names
+	root_category = Category.create(name:root_category_name)
+	puts 'Creating root category... ' + root_category.name + ' ' + root_category.id.to_s
+	root_categories << root_category
+end
+
+for root_category in root_categories
+	for sub_category_name in sub_category_names[root_category.name]
+		sub_category = Category.new({:name => sub_category_name})
+		puts 'Creating sub category... ' + sub_category.name + ' ' + sub_category.id.to_s
+		root_category.categories << sub_category
+	end
+	puts 'ATTEMPING TO SAVE'
+	if root_category.save
+		puts 'Save success!'
+	else
+		puts 'Save failed!'
+	end
+end 
+
+
+
+
