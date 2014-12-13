c6646d20f74ee77dc87857f0f872b1f728332fff
ccleung <clemsquared@gmail.com>
2014-12-13 12:03:42 -0500

    beautify

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -71,13 +71,13 @@ class LocationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_location
-      @location = Location.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_location
+    @location = Location.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def location_params
-      params.require(:location).permit(:city, :country)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def location_params
+    params.require(:location).permit(:city, :country)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :load_categories, only: [:show, :edit, :update, :new, :index]
+  before_action :load_categories, only: [:show, :edit, :update, :new, :index]
 
   # GET /posts
   # GET /posts.json
@@ -7,75 +7,75 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-	def new
-		@post = Post.new
-	end
+  def new
+    @post = Post.new
+  end
 
-	def create
-		#render plain: params[:post].inspect
-		#render plain: current_user.id
-		
-		@post = Post.new
-		@post.title = post_params[:title]
-		@post.description = post_params[:description]
-		@post.user_id = current_user.id
+  def create
+    #render plain: params[:post].inspect
+    #render plain: current_user.id
 
-		#if image exists we need to create a new image
-		if post_params[:image]
-			@image = Image.new
-			@image.image = post_params[:image]
-			@image.imageable = @post
-			@image.save
-		end
+    @post = Post.new
+    @post.title = post_params[:title]
+    @post.description = post_params[:description]
+    @post.user_id = current_user.id
 
-		#if location exists we need to create a new location
-		if post_params[:location]
-			#city, region, country
-			locationInfo = post_params[:location].split(",")
+    #if image exists we need to create a new image
+    if post_params[:image]
+      @image = Image.new
+      @image.image = post_params[:image]
+      @image.imageable = @post
+      @image.save
+    end
 
-			#@location = nil
-			if (locationInfo.count == 3)
-				conditions = { :city => locationInfo[0].strip, 
-		               :region => locationInfo[1].strip,
-		               :country => locationInfo[2].strip }
-				@location = Location.find_or_create_by(conditions)
-				@post.location_id = @location.id
-			end
-	  end
+    #if location exists we need to create a new location
+    if post_params[:location]
+      #city, region, country
+      locationInfo = post_params[:location].split(",")
 
-	  @post.category_id = post_params[:category]
+      #@location = nil
+      if (locationInfo.count == 3)
+        conditions = { :city => locationInfo[0].strip,
+                       :region => locationInfo[1].strip,
+                       :country => locationInfo[2].strip }
+        @location = Location.find_or_create_by(conditions)
+        @post.location_id = @location.id
+      end
+    end
+
+    @post.category_id = post_params[:category]
 
     respond_to do |format|
-			if @post.save
-	      format.html { redirect_to @post, notice: 'Post was successfully created.' }
-	      format.json { render :show, status: :created, location: @post }
-	    else
-	      format.html { render :new }
-	      format.json { render json: @post.errors, status: :unprocessable_entity }
-	    end
-	  end
-	end
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-	def show
-		#render plain: params
-		#params[:id] is now the slug
-		@post = Post.find_by_slug(params[:id])
-		#render plain: [request.path, post_path(@post)]
+  def show
+    #render plain: params
+    #params[:id] is now the slug
+    @post = Post.find_by_slug(params[:id])
+    #render plain: [request.path, post_path(@post)]
 
-		#make sure we use the most recent url path even when the slug has changed
-		if request.path != post_path(@post)
-			render plain: 'hello world'
-			redirect_to @post, status: :moved_permanently
-		end
-	end
+    #make sure we use the most recent url path even when the slug has changed
+    if request.path != post_path(@post)
+      render plain: 'hello world'
+      redirect_to @post, status: :moved_permanently
+    end
+  end
 
-	private
-		def load_categories
-			@categories = Category.all
-		end
+  private
+  def load_categories
+    @categories = Category.all
+  end
 
-		def post_params
-			params.require(:post).permit(:title, :description, :image, :location, :category)
-		end
-		before_action :authenticate_user!
+  def post_params
+    params.require(:post).permit(:title, :description, :image, :location, :category)
+  end
+  before_action :authenticate_user!
 end
