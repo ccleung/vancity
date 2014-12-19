ace597e2ebee8f4fb8da2f1a82e112fa9a260c59
ccleung <clemsquared@gmail.com>
2014-12-18 20:35:22 -0500

    guarantee load of google maps api before using it. Style new post using bootstrap

---
+++ app/assets/javascripts/loadGoogleMaps.js
@@ -0,0 +1,18 @@
+$( function() {
+  if (typeof google == "undefined") {
+    jQuery.getScript("http://maps.google.com/maps/api/js?sensor=false&libraries=places&language=en-US&callback=initialize")
+  } 
+  else {
+    initialize();
+  }
+});
+
+function initialize() {
+  var options = {
+    types: ['(cities)']
+  };
+  var autocomplete = new google.maps.places.Autocomplete($("#location")[0], options);
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var place = autocomplete.getPlace();
+  });
+}
