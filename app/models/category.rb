582de9dad92b12398b362d493fb08c7b0cdee57b
ccleung <clemsquared@gmail.com>
2014-12-20 12:34:10 -0500

    link to posts

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
 	belongs_to :category
 	has_many :categories
+  has_many :posts
 
 	validates_presence_of :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root to: 'visitors#index'
   get 'home' => 'visitors#home'
+  get '/categories/:id/posts', to: 'categories#posts', as: 'category_posts'
   devise_for :users
   resources :users
   resources :posts
